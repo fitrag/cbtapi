f5bd37b9bfd9e61820a81d67bc392e266633669b
fitrag <jaya88114@gmail.com>
2025-01-22 16:03:34 +0700

    update

--- app/Http/Controllers/Api/JawabanController.php
+++ app/Http/Controllers/Api/JawabanController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Jawaban;
+use App\Models\{Jawaban, Soal};
 use App\Http\Resources\JawabanResource;
 
 class JawabanController extends Controller
 {
     public function store(Request $req){
+        $kunci = Soal::find($req->soal_id);
+        
         $insert = Jawaban::updateOrCreate(
             [
                 'soal_id' => $req->soal_id,
@@ -19,12 +21,28 @@ class JawabanController extends Controller
                 'jawaban'   => $req->jawaban,
                 'user_id'   => $req->user_id,]
             );
-        if($insert){
-            return new JawabanResource(true, "Berhasil menjawab", [
-                'jawaban'   => $req->jawaban,
-                'soal_id'   => $req->soal_id,
-            ]);
+
+        if($kunci->kunci == $req->jawaban){
+            if($insert){
+                return new JawabanResource(true, "Berhasil menjawab", [
+                    'status'    => 'Benar',
+                    'jawaban'   => $req->jawaban,
+                    'soal_id'   => $req->soal_id,
+                    'skor'      => $kunci->bobot
+                ]);
+            }
+
+        }else{
+            if($insert){
+                return new JawabanResource(true, "Berhasil menjawab", [
+                    'status'    => 'Salah',
+                    'jawaban'   => $req->jawaban,
+                    'soal_id'   => $req->soal_id,
+                    'skor'      => $kunci->bobot
+                ]);
+            }
         }
+        
     }
 
     public function show($id, $user_id){
---
+++ resources/views/barang.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Haloo</h1>
+
+@endsection
